69fd10daba650ebaab1525126d32a8e59a4fee35
Ilya Koshkin <i.koshkin@emcd.io>
2024-12-13 13:22:49 +0300

    TODO change fixtures add manual headless flag add first version of readme

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,45 +1,62 @@
-from typing import Dict
-
 import pytest
 from pages.locators.welcome import WelcomePage
-from _pytest.fixtures import SubRequest
-from playwright.sync_api import Page, Playwright
-from common.const import Constants
+from playwright.sync_api import Page, expect, sync_playwright
 
 
 @pytest.fixture(scope="function", autouse=True)
-def stand_login(page: Page, request: SubRequest, base_url: str):
+def stand_login(page: Page, base_url: str):
     page.goto(base_url)
-    page.locator(WelcomePage.input_login).fill('Admin')
-    page.locator(WelcomePage.input_password).fill('1111')
-    page.click(WelcomePage.login_submit)
+    page.locator(f"id={WelcomePage.input_login}").click()
+    page.keyboard.type('Admin')
+    page.locator(f"id={WelcomePage.input_password}").click()
+    page.keyboard.type('1111')
+    page.locator('button:text("Войти")').click()
+    expect(page).to_have_title('Nexign UI')
 
 
-@pytest.fixture(scope="function")
-def browser_context_args(
-        browser_context_args: Dict, base_url: str, request: SubRequest
-):
-    context_args = {
-        **browser_context_args,
-        "no_viewport": True,
-        "user_agent": Constants.AUTOMATION_USER_AGENT,
-    }
-
-    if hasattr(request, "param"):
-        context_args["storage_state"] = {
-            "cookies": [
-                {
-                    "name": "session-username",
-                    "value": request.param,
-                    "url": base_url,
-                }
-            ]
-        }
-    return context_args
+def pytest_addoption(parser):
+    parser.addoption(
+        "--headless", action="store_true", default=False, help="headless mode"
+    )
 
 
 @pytest.fixture(scope="session")
-def browser_type_launch_args(browser_type_launch_args: Dict, playwright: Playwright):
+def context(request):
+    playwright = sync_playwright().start()
+    browser = playwright.chromium.launch(headless=request.config.getoption("--headless"))
+    yield browser
+#
+# import pytest
+# from playwright.sync_api import sync_playwright, Page, APIRequestContext
+#
+#
+# @pytest.fixture(scope="function")
+# def browser_context():
+#     with sync_playwright() as p:
+#         browser = p.chromium.launch(channel='chrome', headless=False)
+#         context_options = {
+#             "user_agent": "Chrome/131.0.6778.85",
+#             "viewport": {"width": 1920, "height": 1080},
+#             "locale": "en-US",
+#             "timezone_id": "Europe/Moscow",
+#             "permissions": ["geolocation"]
+#         }
+#
+#         context = browser.new_context(**context_options)
+#         yield context
+#         context.close()
+#         browser.close()
 
-    playwright.selectors.set_test_id_attribute("data-test")
-    return {**browser_type_launch_args, "args": ["--start-maximized"]}
+#
+# @pytest.fixture(scope="function")
+# def browser(browser_context) -> Page:
+#     page = browser_context.new_page()
+#     yield page
+#     page.close()
+#
+#
+# @pytest.fixture(scope="function")
+# def api_request_context(browser_context) -> APIRequestContext:
+#     request_context = browser_context.request
+#     yield request_context
+#     request_context.dispose()
