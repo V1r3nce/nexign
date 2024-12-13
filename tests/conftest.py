2dcc9944bab270a536230a93e9a98f984d94a5f7
Ilya Koshkin <i.koshkin@emcd.io>
2024-12-13 14:03:27 +0300

    review fix delete commented code block

--- tests/conftest.py
+++ tests/conftest.py
@@ -25,38 +25,3 @@ def context(request):
     playwright = sync_playwright().start()
     browser = playwright.chromium.launch(headless=request.config.getoption("--headless"))
     yield browser
-#
-# import pytest
-# from playwright.sync_api import sync_playwright, Page, APIRequestContext
-#
-#
-# @pytest.fixture(scope="function")
-# def browser_context():
-#     with sync_playwright() as p:
-#         browser = p.chromium.launch(channel='chrome', headless=False)
-#         context_options = {
-#             "user_agent": "Chrome/131.0.6778.85",
-#             "viewport": {"width": 1920, "height": 1080},
-#             "locale": "en-US",
-#             "timezone_id": "Europe/Moscow",
-#             "permissions": ["geolocation"]
-#         }
-#
-#         context = browser.new_context(**context_options)
-#         yield context
-#         context.close()
-#         browser.close()
-
-#
-# @pytest.fixture(scope="function")
-# def browser(browser_context) -> Page:
-#     page = browser_context.new_page()
-#     yield page
-#     page.close()
-#
-#
-# @pytest.fixture(scope="function")
-# def api_request_context(browser_context) -> APIRequestContext:
-#     request_context = browser_context.request
-#     yield request_context
-#     request_context.dispose()
