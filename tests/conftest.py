d0c014e4d6f94609a010f061bf19cc3e23eec3fd
Shlyahov Alexandr <ashlyahov@usetech.ru>
2025-01-31 12:34:56 +0000

    поддерка MOON и SELENOID

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,28 +1,49 @@
 import os
+import urllib.parse
 
 import allure
 import pytest
 
 from common.const import Constants
 from common.helpers.download_helper import CheckFile
-from common.helpers.env_helper import BASE_URL_API, UserData, BASE_URL
+from common.helpers.env_helper import BASE_URL_API, UserData, BASE_URL, get_var_from_env
 from playwright.sync_api import Page, APIRequestContext, expect, Playwright, BrowserContext
+from importlib.metadata import version
 
 from common.helpers.time_helpers import get_now_time
 from pages.locators.login_page import LoginForm
 
+remote_driver = get_var_from_env("REMOTE_DRIVER")
+test_run_mode = get_var_from_env("TEST_RUN_MODE")
 
-os.environ['SELENIUM_REMOTE_CAPABILITIES'] = \
-    f'''
-    {{"selenoid:options":
-        {{
-            "name":"{get_now_time()}", 
-            "enableVNC": true, 
-            "enableVideo": false, 
-            "sessionTimeout": "5m"
+if remote_driver == "SELENOID" and test_run_mode == "remote":
+    os.environ["SELENIUM_REMOTE_URL"] = "http://srv8-triptindus:4444/wd/hub"
+    os.environ['SELENIUM_REMOTE_CAPABILITIES'] = \
+        f'''
+        {{"selenoid:options":
+            {{
+                "name":"{get_now_time()}", 
+                "enableVNC": true, 
+                "enableVideo": false, 
+                "sessionTimeout": "5m"
+            }}
         }}
-    }}
-    '''
+        '''
+
+@pytest.fixture()
+def moon_url_with_params(request):
+    if remote_driver == "MOON":
+        remote_driver_host = get_var_from_env(f"{remote_driver}_HOST")
+
+        moon_url = f"ws://{remote_driver_host}/playwright/chrome/playwright-{version('playwright')}"
+
+        params = {
+            'headless': f"{request.config.getoption('--headless')}",
+            'name': f"{request.node.name} | {get_now_time()}"
+        }
+        url_params = urllib.parse.urlencode(params, doseq=True)
+        ws_url = f"{moon_url}?{url_params}"
+        return ws_url
 
 
 def pytest_addoption(parser):
@@ -30,12 +51,27 @@ def pytest_addoption(parser):
         "--headless", action="store_true", default=False, help="headless mode"
     )
 
+@pytest.fixture()
+def get_browser(request, playwright: Playwright, moon_url_with_params):
+    """Фикстура отвечающая за запуск браузера, в зависимости от режима запуска (локальный или удаленный)
+    и удаленного драйвера (если выбран удаленный режим)"""
+
+    if test_run_mode == "local":
+        browser = playwright.chromium.launch(channel='chrome', headless=request.config.getoption("--headless"),
+                                             args=["--start-maximized"])
+    else:
+        if remote_driver == "MOON":
+            browser = playwright.chromium.connect(moon_url_with_params)
+        else:
+            browser = playwright.chromium.launch(channel='chrome', headless=request.config.getoption("--headless"),
+                                                 args=["--start-maximized"])
+
+    return browser
 
 @pytest.fixture(scope="function")
-def context(playwright: Playwright, request) -> BrowserContext:
-    browser = playwright.chromium.launch(channel='chrome', headless=request.config.getoption("--headless"),
-                                         args=["--start-maximized"])
-    context = browser.new_context(no_viewport=True)
+def context(request, get_browser) -> BrowserContext:
+    browser = get_browser
+    context = browser.new_context(no_viewport=False if remote_driver == "MOON" else True)
     yield context
     context.close()
     browser.close()
@@ -44,6 +80,7 @@ def context(playwright: Playwright, request) -> BrowserContext:
 @pytest.fixture(scope="function")
 def page(context: BrowserContext) -> Page:
     page = context.new_page()
+    if remote_driver == "MOON": page.set_viewport_size({"width": 1920, "height": 1080})
     page.set_default_timeout(Constants.DEFAULT_TIMEOUT)
     yield page
     page.close()
