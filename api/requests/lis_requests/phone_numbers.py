e7a0d53937b5619ef5139f3aa9f5dbb1fc139be3
Sidorov Andrey <andrey.sidorov.v@simbirsoft.com>
2025-02-03 08:48:23 +0000

    TUDS-2222

--- api/requests/lis_requests/phone_numbers.py
+++ api/requests/lis_requests/phone_numbers.py
@@ -7,12 +7,19 @@ class PhoneNumbersRequests:
         self.api_request_auth_context = api_request_auth_context
 
     @allure.step("Получить список телефонных номеров LIS")
-    def get_phone_numbers(self, server_url: str, type_def: bool = True):
+    def get_phone_numbers(self, server_url: str, type_def: bool = True, status_id: [None, list] = None,
+                          state_id: [None, list] = None, num_sort: [None, str] = None):
         """
         Получить список телефонных номеров LIS
         """
         payload = {"returnCount": True, "macroRegionIds": [1], "isTypeDEF": type_def, "includeInternalMNP": True}
+        if status_id:
+            payload["statusIds"] = status_id
+        if state_id:
+            payload["stateIds"] = state_id
         params = {"limit": 50, "offset": 0}
+        if num_sort:
+            params["sort"] = num_sort
         phone_numbers = self.api_request_auth_context.post(url=f"{server_url}/OAPI/v1/lis/logicalResources/phoneNumbers/search",
                                                            data=payload, params=params)
         assert phone_numbers.status == 200, "Не получен список телефонных номеров"
--- pages/locators/lis_locators/base_elements_lis.py
+++ pages/locators/lis_locators/base_elements_lis.py
@@ -8,12 +8,17 @@ class BaseElementsLis:
     def __init__(self, page: Page):
         self.page = page
 
+        self.ADD_BUTTON = Element("//ps-button[contains(@on-submit, 'onFormSubmit()')]", "Кнопка 'Добавить'", self.page)
+
         #MODAL
         self.MODAL = Element("div.n-popup", "Модальное окно", self.page)
-        self.MODAL_X_BTN = Element("[ng-show*='titleButtons.close.visible']", "Кнопка Х закрыть модального окна", self.page)
+        self.MODAL_X_BTN = Element("[ng-show*='titleButtons.close.visible']", "Кнопка Х закрыть модального окна",
+                                   self.page)
         self.MODAL_TITLE = Element("div.n-popup-head__title", "Заголовок модального окна", self.page)
         self.MODAL_BODY_TEXT = Element("div.n-popup-message-text", "Текст модального окна", self.page)
         self.FIRST_BTN = Element("div.n-popup ps-button:first-child", "Первая кнопка модального окна",
                                  self.page)
         self.SECOND_BTN = Element("div.n-popup ps-button:last-child", "Вторая кнопка модального окна",
                                   self.page)
+        self.OK_BTN = Element("//ps-button[contains(text(), 'OK')]", "Кнопка 'ОК'",
+                              self.page)
