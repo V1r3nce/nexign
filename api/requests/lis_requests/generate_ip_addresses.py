3422f3f6f76fc586689e279c48c407ffbeafc4b0
Vladislav Khrabrykh <Vladislav.Khrabrykh@nexign.com>
2025-03-11 14:52:02 +0300

    fix/khrabrykh/TUDS-2413

--- api/requests/lis_requests/generate_ip_addresses.py
+++ api/requests/lis_requests/generate_ip_addresses.py
@@ -31,7 +31,7 @@ def generate_ip_addresses(api_request_auth_context: APIRequestContext, base_url_
     assert response.status == 204, (
             f"Не выполнен запрос на создание нового(-ых) IP-адреса в LIS.\n"
             f"Status: {response.status}\n"
-            f"Message: {response.json().get("userMessage", response.text())}"
+            f"Message: {response.json().get('userMessage', response.text())}"
         )
     
     return ip_list[0] if ip_count == 1 else ip_list
