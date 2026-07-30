04e528268152e17aa8159cb443d4e9fb4aea912d
Seluyanov Denis <Denis.Seluyanov@nexign.com>
2026-07-31 02:45:53 +0300

    E2E_43_3: тест 01 на одном продукте Терминал L

--- tests/nbss/e2e_43_3_taxes_display/test_display_taxes_during_sale.py
+++ tests/nbss/e2e_43_3_taxes_display/test_display_taxes_during_sale.py
@@ -36,7 +36,6 @@ class TestDisplayTaxesDuringSale:
         self.create_request_form = CreateSalesAndServiceManagement()
         self.client = create_organization_with_agreement_and_account
         self.discount_percent = 20
-        self.PRODUCT_RENT = product_names_map[B2BProducts.satellite_rent_alt]
         self.PRODUCT_SALE = product_names_map[B2BProducts.equipment_sale]
         self.OPTION_NAME = "+2 ГБ"
 
@@ -70,60 +69,42 @@ class TestDisplayTaxesDuringSale:
         with allure.step("Создание заявки продажи"):
             self.create_sale_inquiry()
 
-        with allure.step("Выбор продуктовых предложений и проверка налога в детальной информации о продукте"):
+        with allure.step("Выбор ПП: проверка налога на вкладке 'Цены' детальной информации о продукте"):
             test_context.client.inquiry = prepare_inquiries(
-                category=["satellite_rent", "equipment_sale"],
-                product_offering_id=[B2BProducts.satellite_rent_alt, B2BProducts.equipment_sale],
-                as_list=False,
+                category=["equipment_sale"], product_offering_id=[B2BProducts.equipment_sale], as_list=False
             )
-            products = {product.product_name: product for product in test_context.client.inquiry.product_list}
+            product = test_context.client.inquiry.product
 
             self.inquiries_page.locators.ADD_SALE_BTN.click()
             self.inquiries_page.search_products_in_form(
-                product_offer_name=self.PRODUCT_RENT, product_category_name="Спутниковая связь"
+                product_offer_name=self.PRODUCT_SALE, product_category_name="Товары и оборудование"
             )
-            # self.inquiries_page.check_product_details_taxes(product_offer_name=self.PRODUCT_RENT)
-            self.inquiries_page.add_found_product_to_commercial_order(products[self.PRODUCT_RENT])
+            self.inquiries_page.check_product_details_taxes(product_offer_name=self.PRODUCT_SALE)
+            self.inquiries_page.add_found_product_to_commercial_order(product)
 
-            self.inquiries_page.add_product_offer_to_commercial_order(products[self.PRODUCT_SALE])
-            products[self.PRODUCT_RENT].switch_name = "Коммутатор_Спутниковая_связь"
-            products[self.PRODUCT_SALE].switch_name = "Коммутатор_Спутниковая_связь"
-
-        with allure.step("Проверка налога во всплывающих подсказках 'Итого' до применения скидки"):
+        with allure.step("Проверка налога во всплывающей подсказке 'Итого' до применения скидки"):
             self.inquiries_page.check_total_payment_tax_tooltip(fee_type="one_time")
-            self.inquiries_page.check_total_payment_tax_tooltip(fee_type="subscription")
 
         with allure.step("Проверка отображения налога на форме 'Назначение скидок'"):
             self.inquiries_page.open_mass_discount_assignment_form()
-            # self.inquiries_page.check_taxes_in_mass_discount_form(
-            #     product_offering_id=B2BProducts.equipment_sale, fee_type="one_time"
-            # )
-            # self.inquiries_page.check_taxes_in_mass_discount_form(
-            #     product_offering_id=B2BProducts.satellite_rent_alt, fee_type="subscription"
-            # )
+            self.inquiries_page.check_taxes_in_mass_discount_form(
+                product_offering_id=B2BProducts.equipment_sale, fee_type="one_time"
+            )
 
-        with allure.step("Назначить скидки продуктам заказа"):
-            self.inquiries_page.fill_discounts_on_mass_discount_assignment_form([self.discount_percent])
+        with allure.step("Назначить скидку продукту заказа"):
             self.inquiries_page.fill_one_time_discounts_on_mass_discount_assignment_form([self.discount_percent])
             self.inquiries_page.save_discounts_on_mass_discount_assignment_form()
 
-        with allure.step("Проверка налога во всплывающих подсказках 'Итого' после применения скидки"):
+        with allure.step("Проверка налога во всплывающей подсказке 'Итого' после применения скидки"):
             self.inquiries_page.check_total_payment_tax_tooltip(fee_type="one_time")
-            self.inquiries_page.check_total_payment_tax_tooltip(fee_type="subscription")
 
-        with allure.step("Проверка пересчитанного налога на вкладке 'Цены' формы редактирования продуктов"):
-            self.inquiries_page.open_product_price_tab_by_price_link(fee_type="subscription")
-            self.inquiries_page.check_taxes_on_price_tab(fee_type="subscription")
-            self.inquiries_page.close_edit_product_form()
-
+        with allure.step("Проверка пересчитанного налога на вкладке 'Цены' формы редактирования продукта"):
             self.inquiries_page.open_product_price_tab_by_price_link(fee_type="one_time")
             self.inquiries_page.check_taxes_on_price_tab(fee_type="one_time")
             self.inquiries_page.close_edit_product_form()
 
         with allure.step("Завершение продажи"):
-            self.inquiries_page.auto_reserve_all_resources(
-                [products[self.PRODUCT_RENT].category, products[self.PRODUCT_SALE].category]
-            )
+            self.inquiries_page.auto_reserve_all_resources(product.category)
             self.inquiries_page.check_configuration()
             self.inquiries_page.locators.NEXT_STEP_BTN.click()
             self.inquiries_page.wait_connect_package_offers_and_close_inquiry(
